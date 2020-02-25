3a921993d6b75c9df584ce42e4127db3d5c911b9
WillzMu <wilfredmulenga@yahoo.com>
2020-02-25 21:33:38 +0200

    refactor: refactor response so to verify webhook

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/webhook', (req, res) => {
     res.status(200).send('EVENT_RECEIVED')
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404)
+    res.status(200).send('EVENT_RECEIVED')
   }
 })
 
