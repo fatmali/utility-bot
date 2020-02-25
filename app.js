e5032ae75a1af775416da5f13b9e27cb718d6494
WillzMu <wilfredmulenga@yahoo.com>
2020-02-25 21:51:10 +0200

    refactor: install dotenv

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/webhook', (req, res) => {
     res.status(200).send('EVENT_RECEIVED')
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
-    res.status(200).send('EVENT_RECEIVED')
+    res.sendStatus(404)
   }
 })
 
