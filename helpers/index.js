e9e6cbd1ee777f13f93fc3149fdd8a16c09f022b
WillzMu <wilfredmulenga@yahoo.com>
2020-03-07 21:43:44 +0200

    refactor: test share location quick reply

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,7 @@
 
 const request = require('./request')
 const constants = require('../constants')
-const { welcomeMessage, sharePhoto, addDetailsQuickReply, requestToAddDetails, misunderstoodReply, reportCompletedResponse } = require('../sendApi/messages')
+const { welcomeMessage, requestToShareLocation, sharePhoto, addDetailsQuickReply, requestToAddDetails, misunderstoodReply, reportCompletedResponse } = require('../sendApi/messages')
 
 async function callSendAPI (sender_psid, response) {
   // Construct the message body
@@ -33,7 +33,7 @@ function handlePostback (sender, postback) {
         callSendAPI(sender.id, welcomeMessage)
         break
       case constants.REPORT:
-        callSendAPI(sender.id, sharePhoto)
+        callSendAPI(sender.id, requestToShareLocation)
         break
       case constants.ADD_DETAILS.YES:
         callSendAPI(sender.id, requestToAddDetails)
