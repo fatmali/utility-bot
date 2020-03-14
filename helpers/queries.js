e8ad94bd655d5439d1b946415a329620a07d4a50
WillzMu <wilfredmulenga@yahoo.com>
2020-03-14 23:42:26 +0200

    refactor: test senderID

--- helpers/queries.js
+++ helpers/queries.js
@@ -2,13 +2,13 @@ const { Client } = require('pg')
 require('dotenv').config()
 
 const pgClient = new Client({
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: true
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+  // user: process.env.PGUSER,
+  // host: process.env.PGHOST,
+  // database: process.env.PGDATABASE,
+  // password: process.env.PGPASSWORD,
+  // port: process.env.PORT
 })
 
 module.exports = { pgClient }
